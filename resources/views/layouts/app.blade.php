b203bb5a42498010e664d08b2501bbe54ca6066b
Abdeljalil <zeronada@hotmail.com>
2019-05-14 12:23:13 +0200

    mise à jour du projet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body>  
 
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #636b6f;   
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
