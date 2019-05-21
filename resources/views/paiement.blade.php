3caf816846884684f73285184fd002ca1538dea4
bilal1040 <bilal-rimo@hotmail.com>
2019-05-21 10:01:31 +0200

    Paiement

--- resources/views/paiement.blade.php
+++ resources/views/paiement.blade.php
@@ -1,50 +1,111 @@
-  <div>
-    <h1 style="text-align: center; margin-top:5%;"> Panier </h1><br />
 
-    <div style="margin-left: 19%;">
-      <table style="width: 80%;">
-        <tr>
-          <th style="width:35%; border:1px solid black"> Titre </th>
-          <th style="width:10%; border:1px solid black"> Nombre de place </th>
-          <th style="width:10%; border:1px solid black"> Prix</th>
 
-          <tr>
-            <td>titre (BD°</td>
-            <td>0(js)</td>
-            <td>php + innerHTML</td>
-          </tr>
-          <tr>
-            <td>titre (BD°</td>
-            <td>0(js)</td>
-            <td>php + innerHTML</td>
-          </tr>
-          <tr>
-            <td>titre (BD°</td>
-            <td>0(js)</td>
-            <td>php + innerHTML</td>
-          </tr>
-        </tr>
-      </table>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+  <h1 id="titreArtiste"> Panier </h1>
+  <br />
+
+  <div class="container">
+    <div class="row">
+      <div class="col">      
+      </div>
+      <div class="col-10">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col"> Titre </th>
+              <th scope="col"> Nombre de place </th>
+              <th scope="col"> Prix</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <?php
+            $conn = mysqli_connect("localhost","root","","reservations");
+            // Check connection
+            if (mysqli_connect_errno()){
+              echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            }
+            $total =0;
+            $nomPlace =1;
+            $tpc = 0;
+            //Button when I clikc on submit
+            if (isset($_GET['submit'])){
+              foreach ($_GET['id'] as $id):
+                //Select all From users
+                $sq1=mysqli_query($conn,"select * from `users`");
+                $srow1=mysqli_fetch_array($sq1);
+                //Select shows when Id select equal to id 
+                $sq=mysqli_query($conn,"select * from `shows` where id='$id'");
+                $srow=mysqli_fetch_array($sq);
+                //Variable define
+                $idUsers = $srow1['id'];
+                $total += $srow['price'];
+                $title = $srow['title'];
+                $prix = $srow['price'];
+                $idShows = $srow['id'];
+                $tpc = $total *100;
+          ?>
+          <tbody>
+            <tr>            
+              <td> <?php echo $title; ?> </td>
+              <td> <p id="btnEdit">1</p></td>
+              <td><?php echo $prix; ?></td>
+              <td><button onclick="edit()">Modifier</button></td>
+            </tr>
+          </tbody>
+          <?php
+            endforeach;             
+            }
+          ?>
+        </table>      
+      </div>
+     </div>
+    <div style="text-align: right; width:80%; margin-top:2%;">
+      <p><b> <?php echo $total; ?> </b></p>
     </div>
-  </div>
+    <!-- this form render to orderController for show the modal payment -->
+    <div id="payment">
+      <form action="paiementconfirm" method="GET">
+        <script
+          src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+          data-key="pk_test_jLKK1NFImPeoJm9q6y85K7Wp"
+          data-amount="<?php echo $tpc; ?>"
+          data-name="Projet Reservation"
+          data-description="Facture"
+          data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+          data-locale="auto"
+          data-currency="eur">
+        </script>
+       <?php
+        
+        $conn = mysqli_connect("localhost","root","","reservations");
+        // Check connection
+        if (mysqli_connect_errno())
+        {
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        $sql = "INSERT INTO `reservation`(`quantite`, `users_id`, `shows_id`) VALUES ('".$nomPlace."','".$idUsers."','".$idShows."')";
+        if (isset($_GET['submit'])){
+          if(mysqli_query($conn,$sql)){
+            echo "Records inserted successfully.";
+          } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+          }
+        }
+      ?>
 
-  <div style="text-align: right; width:80%; margin-top:2%;">
-    <p><b> Total via JS </b></p>
-  </div>
+      </form>
 
-  <!-- this form render to orderController for show the modal payment -->
-  <div id="payment">
-    <form action="paiementconfirm" method="GET">
-      <script
-        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-        data-key="pk_test_jLKK1NFImPeoJm9q6y85K7Wp"
-        data-amount="2250"
-        data-name="Projet Reservation"
-        data-description="Facture"
-        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-        data-locale="auto"
-        data-currency="eur">
-      </script>
-    </form>
-    
-</div>
+    </div>
+
+    <script>
+      function edit() {
+        var nmbr = prompt('Entrez le nombre de place');
+        document.getElementById("btnEdit").innerHTML = nmbr;
+      }
+
+    </script>
+    <div class="col">
+    </div>
+    </div>
+
+
--- resources/views/shows/index.blade.php
+++ resources/views/shows/index.blade.php
@@ -8,41 +8,63 @@
 
 <div class="container">
   <div class="row">
-    <div class="col">
-      
-    </div>
-   
-
-    <div class="col-10">
-<table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Slug</th>
-      <th scope="col">Title</th>
-      <th scope="col">Poster_url</th>
-      <th scope="col">Bookable</th>
-      <th scope="col">Price</th>
-      <th scope="col">Location_id</th>
-    </tr>
-  </thead>
-  <tbody>
-        @foreach($shows as $show)
+    <div class="col">      
+  </div>
+  <div class="col-10">
+    <form method="GET" action="paiement">
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Choix</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Title</th>
+            <th scope="col">Poster_url</th>
+            <th scope="col">Bookable</th>
+            <th scope="col">Price</th>
+            <th scope="col">Location_id</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!--  @foreach($shows as $show)
+                <tr>
+                    <td><input type="checkbox" name='list' value="<?php echo $show['id']; ?>"></td>
+                    <td>{{ $show->slug }}</td>
+                    <td>{{ $show->title }}</td>
+                    <td>{{ $show->poster_url }}</td>
+                    <td>{{ $show->bookable}}</td>
+                    <td>{{ $show->price}}</td>
+                    <td>{{ $show->location_id }}</td>
+                </tr>
+            @endforeach-->		
+            <?php
+              $conn = mysqli_connect("localhost","root","","reservations");
+              // Check connection
+              if (mysqli_connect_errno()){
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+              }
+              $query=mysqli_query($conn,"select * from `shows`");
+              while($row=mysqli_fetch_array($query)){
+            ?>
             <tr>
-                <td>{{ $show->slug }}</td>
-                <td class="col-">{{ $show->title }}</td>
-                <td>{{ $show->poster_url }}</td>
-                <td>{{ $show->bookable}}</td>
-                <td>{{ $show->price}}</td>
-                <td>{{ $show->location_id }}</td>
+              <td><input type="checkbox" value="<?php echo $row['id']; ?>" name="id[]"></td>
+              <td><?php echo $row['slug']; ?></td>
+              <td><?php echo $row['title']; ?></td>
+              <td><?php echo $row['poster_url']; ?></td>
+              <td><?php echo $row['bookable']?></td>
+              <td><?php echo $row['price']?></td>
+              <td><?php echo $row['location_id']?></td>
             </tr>
-        @endforeach
+            <?php
+              }
+            ?>
         </tbody>
-    </table>
+      </table>
+      <input type="submit" name="submit" value="Submit">
+    </form>
 
     </div>
-        <div class="col">
-            
-        </div>
+      <div class="col">
     </div>
+  </div>
 </div>
 @endsection
