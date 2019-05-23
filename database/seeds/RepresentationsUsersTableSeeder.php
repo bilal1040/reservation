d5a8a77d22f6448025f20c90dad41bd206413750
Abdeljalil <zeronada@hotmail.com>
2019-05-24 00:29:08 +0200

    suppresion d'un doublon de create_reservation_table+reglage conclict avec RepresentationUsersTableSeeder

--- database/seeds/RepresentationsUsersTableSeeder.php
+++ database/seeds/RepresentationsUsersTableSeeder.php
@@ -12,31 +12,24 @@ class RepresentationsUsersTableSeeder extends Seeder
     public function run()
     {
         $representationsusers = [
-        	['places' =>'50',
-        	'users_id'=>'2',
-        	'representation_id'=>'1',
-        ],
 
-        	['places'=>'70',
-<<<<<<< HEAD
-        	'users_id'=>'1',
-        	'representation_id'=>'4',
-    	],	
+            [
+                'places' =>'50',
+            	'users_id'=>'2',
+            	'representation_id'=>'1',
+            ],
+            
+            [
+                'places'=>'70',
+            	'users_id'=>'1',
+            	'representation_id'=>'4',
+        	],	
 
-    		['places'=>'20',
-    		'users_id'=>'1',
-    		'representation_id'=>'2',
-=======
-        	'users_id'=>'2',
-        	'representation_id'=>'2',
-    	],	
-
-    		['places'=>'20',
-    		'users_id'=>'2',
-    		'representation_id'=>'3',
->>>>>>> 1b535af660a40e8953ef42fdec51bf637445d56f
-
-    	],
+        	[
+                'places'=>'20',
+        		'users_id'=>'1',
+        		'representation_id'=>'2',
+        	],
 
 
 
