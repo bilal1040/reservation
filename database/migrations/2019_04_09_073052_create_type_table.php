ba56ffc084884055f0e510fe4878967fe140986b
Oussama Husain <oussama.husain@gmail.com>
2019-05-26 15:26:14 +0200

    Ordre de la migration modiffier, ajout de la migration locations & suppression du controller LocationsController (avec un s).

--- database/migrations/2019_04_09_073052_create_type_table.php
+++ database/migrations/2019_04_09_073052_create_type_table.php
@@ -14,7 +14,7 @@ class CreateTypeTable extends Migration
     public function up()
     {
         Schema::create('types', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('type')->length(60);
         });
     }
